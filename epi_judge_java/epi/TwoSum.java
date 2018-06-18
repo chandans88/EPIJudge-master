37959e6e48c41baf23c333d80ea12d8d96666de7
Sathya <csathya@csri1hdc001.office.comscore.com>
2018-06-18 10:55:23 -0400

    Changes from laptop - test

--- epi_judge_java/epi/TwoSum.java
+++ epi_judge_java/epi/TwoSum.java
@@ -9,8 +9,17 @@ public class TwoSum {
   @EpiTest(testfile = "two_sum.tsv")
 
   public static boolean hasTwoSum(List<Integer> A, int t) {
-    // Implement this placeholder.
-    return true;
+  
+    HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+    for(int a : A){
+      map.put(t-a, a);
+    }
+    for(int a : A){
+      if(map.getKey(a)){
+        return true;
+      }
+    }
+  
   }
 
   public static void main(String[] args) {
