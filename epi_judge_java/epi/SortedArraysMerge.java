2f784db4ed9bf54925dff40df7bb6c09f6f16d86
chandans88 <chandans.190788@gmail.com>
2018-06-19 20:29:23 -0400

    More progress

--- epi_judge_java/epi/SortedArraysMerge.java
+++ epi_judge_java/epi/SortedArraysMerge.java
@@ -3,7 +3,7 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-import java.util.List;
+import java.util.*;
 
 public class SortedArraysMerge {
 
@@ -11,9 +11,54 @@ public class SortedArraysMerge {
 
   public static List<Integer>
   mergeSortedArrays(List<List<Integer>> sortedArrays) {
-    // Implement this placeholder.
-    return null;
+
+      //create a list of iterators of size of sortedArrays
+      List<Iterator<Integer>> iters = new ArrayList<>(sortedArrays.size());
+
+      //initialize iterators
+      for(List<Integer> a : sortedArrays)
+          iters.add(a.iterator());
+
+      //create Priority Queue
+      PriorityQueue<ArrayEntry> minHeap = new PriorityQueue<>(sortedArrays.size(), new Comparator<ArrayEntry>() {
+          @Override
+          public int compare(ArrayEntry arrayEntry1, ArrayEntry arrayEntry2) {
+              return Integer.compare(arrayEntry1.value,arrayEntry2.value);
+          }
+      });
+
+      //add to the PQ
+      for(int i=0; i<iters.size(); i++){
+          if(iters.get(i).hasNext())
+              minHeap.add(new ArrayEntry(iters.get(i).next(),i));
+      }
+
+      //create result set
+      List<Integer> result = new ArrayList<>();
+
+      //pick minimal element from PQ
+
+      while(!minHeap.isEmpty()){
+          ArrayEntry topEntry = minHeap.poll();
+          result.add(topEntry.value);
+
+          if(iters.get(topEntry.arrayId).hasNext()){
+              minHeap.add(new ArrayEntry(iters.get(topEntry.arrayId).next(),topEntry.arrayId));
+          }
+      }
+
+      return result;
+
   }
+    public static class ArrayEntry{
+        public Integer value;
+        public Integer arrayId;
+
+        public ArrayEntry(Integer value, Integer arrayId){
+            this.value = value;
+            this.arrayId = arrayId;
+        }
+    }
 
   public static void main(String[] args) {
     System.exit(GenericTest
